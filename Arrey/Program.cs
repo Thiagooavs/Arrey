3c0043923eb18a2287b7d4a6d9bb566b78c81689
Usuario <Usuario@CPD02-M11>
2024-04-30 11:37:31 -0300

    Adicionar arquivos de projeto.

---
+++ Arrey/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*vendas
+             string[] nome = new string[5];
+             double[] comi = new double[5];
+             double[] tot = new double[5];
+
+             for (int i = 0; i <= 4; i++)
+             {
+                 Console.Write("digite o nome do " +(i + 1) + "° vendedor:");
+                 nome[i]= Console.ReadLine();
+
+                 Console.Write("digite o total de vendas do " + (i + 1) + "° vendedor:");
+                 tot[i] = double.Parse(Console.ReadLine());
+
+                if (tot[i] <= 20000)
+                 {
+                     comi[i] = tot[i] * 0.05;   
+                 }
+                 else if (tot[i] <= 40000)
+                 {
+                     comi[i] = tot[i] * 0.06;
+                 }
+                else
+                 {
+                     comi[i] = tot[i] * 0.07;
+                 }
+
+                Console.Clear();
+
+             }
+             for (int j = 0; j <= 4; j++)
+             {
+                 Console.WriteLine("Vendedor " + nome[j] + ": teve um total de vendas de R$" + tot[j] + " e pagara uma comissão de R$" + comi[j]);
+
+
+             }
+            */
+            
+
+            Console.ReadKey();
+        }
+    }
+}
